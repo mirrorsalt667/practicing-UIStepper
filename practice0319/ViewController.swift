07c4732c10b6e3aba8fcb711795e3b9d0e82bc1a
Jpop <jjjjllears@gmail.com>
2021-03-19 22:16:49 +0800

    Initial Commit

---
+++ practice0319/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice0319
+//
+//  Created by 黃肇祺 on 2021/3/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
